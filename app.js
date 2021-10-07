6b382f8d2e4e58735a833fdf99a434be72dd73a1
jyl7094 <jyl7094@gmail.com>
2021-10-07 16:59:04 -0400

    Finish a simple to do list project

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const date = require(__dirname + "/date.js");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const tasks = [];
+const workTasks = [];
+
+app.get("/", (req, res) => {
+    res.render("list", {title: date.getDate(), tasks: tasks});
+});
+
+app.get("/work", (req, res) => {
+    res.render("list", {title: "Work List", tasks: workTasks});
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+}); 
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    if (req.body.list === "Work List") {
+        workTasks.push(req.body.task);
+        res.redirect("/work");
+    } else {
+        tasks.push(req.body.task);
+        res.redirect("/");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
